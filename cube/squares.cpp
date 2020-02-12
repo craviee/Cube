6558f5172cd96e16be2c0cae3432451d5f82a412
craviee <danielcraviee@gmail.com>
2020-02-12 13:54:48 +0100

    Fixing build issues related to old packages

--- cube/squares.cpp
+++ cube/squares.cpp
@@ -1,119 +1,118 @@
 
 #include "opencv2/core/core.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp":
 #include <iostream>
 #include <math.h>
 #include <string.h>
 #include "config.h"
-namespace teste
+
+using namespace cv;
+using namespace std;
+
+static std::string Char2StyleSheet(char c)
 {
-    static std::string Char2StyleSheet(char c)
+    std::string result = "background-color: ";
+    if(c == 'B') return (result + "blue");
+    else if(c == 'W') return (result + "white");
+    else if(c == 'O') return (result + "orange");
+    else if(c == 'R') return (result + "red");
+    else if(c == 'G') return (result + "green");
+    else if(c == 'Y') return (result + "yellow");
+    else return (result + "black");
+}
+static char StyleSheet2Char(std::string c)
+{
+    if(c == "background-color: blue") return 'B';
+    if(c == "background-color: white") return 'W';
+    if(c == "background-color: orange") return 'O';
+    if(c == "background-color: red") return 'R';
+    if(c == "background-color: green") return 'G';
+    if(c == "background-color: yellow") return 'Y';
+    else return 'X';
+}
+static std::vector<std::vector<cv::Point>> ordenaCubinho(std::vector<std::vector<cv::Point>> squares)
+{
+    std::vector<cv::Point> aux;
+    int menor_x_i, menor_x_j, menor_y_i, menor_y_j;
+    for(int i = 0; i < squares.size(); i++)
     {
-        std::string result = "background-color: ";
-        if(c == 'B') return (result + "blue");
-        else if(c == 'W') return (result + "white");
-        else if(c == 'O') return (result + "orange");
-        else if(c == 'R') return (result + "red");
-        else if(c == 'G') return (result + "green");
-        else if(c == 'Y') return (result + "yellow");
-        else return (result + "black");
+        menor_x_i = std::min(squares[i][0].x, std::min(squares[i][1].x , std::min(squares[i][2].x, squares[i][3].x)));
+        menor_y_i = std::min(squares[i][0].y, std::min(squares[i][1].y , std::min(squares[i][2].y, squares[i][3].y)));
     }
-    static char StyleSheet2Char(std::string c)
+    for(int i = 0; i < squares.size(); i++)
     {
-        if(c == "background-color: blue") return 'B';
-        if(c == "background-color: white") return 'W';
-        if(c == "background-color: orange") return 'O';
-        if(c == "background-color: red") return 'R';
-        if(c == "background-color: green") return 'G';
-        if(c == "background-color: yellow") return 'Y';
-        else return 'X';
-    }
-    static std::vector<std::vector<cv::Point>> ordenaCubinho(std::vector<std::vector<cv::Point>> squares)
-    {
-        std::vector<cv::Point> aux;
-        int menor_x_i, menor_x_j, menor_y_i, menor_y_j;
-        for(int i = 0; i < squares.size(); i++)
+        for(int j = i+1; j < squares.size(); j++)
         {
             menor_x_i = std::min(squares[i][0].x, std::min(squares[i][1].x , std::min(squares[i][2].x, squares[i][3].x)));
             menor_y_i = std::min(squares[i][0].y, std::min(squares[i][1].y , std::min(squares[i][2].y, squares[i][3].y)));
-        }
-        for(int i = 0; i < squares.size(); i++)
-        {
-            for(int j = i+1; j < squares.size(); j++)
+            menor_x_j = std::min(squares[j][0].x, std::min(squares[j][1].x , std::min(squares[j][2].x, squares[j][3].x)));
+            menor_y_j = std::min(squares[j][0].y, std::min(squares[j][1].y , std::min(squares[j][2].y, squares[j][3].y)));
+            if((menor_y_j*2 + menor_x_j) < (menor_y_i*2 + menor_x_i))
             {
-                menor_x_i = std::min(squares[i][0].x, std::min(squares[i][1].x , std::min(squares[i][2].x, squares[i][3].x)));
-                menor_y_i = std::min(squares[i][0].y, std::min(squares[i][1].y , std::min(squares[i][2].y, squares[i][3].y)));
-                menor_x_j = std::min(squares[j][0].x, std::min(squares[j][1].x , std::min(squares[j][2].x, squares[j][3].x)));
-                menor_y_j = std::min(squares[j][0].y, std::min(squares[j][1].y , std::min(squares[j][2].y, squares[j][3].y)));
-                if((menor_y_j*2 + menor_x_j) < (menor_y_i*2 + menor_x_i))
-                {
-                    aux = squares[j];
-                    squares[j] = squares[i];
-                    squares[i] = aux;
-                }
+                aux = squares[j];
+                squares[j] = squares[i];
+                squares[i] = aux;
             }
         }
-        return squares;
     }
-    static cv::Scalar colorForLine(char c)
-    {
-        if(c == 'G')
-            return cv::Scalar(0,255,0);
-        else if(c == 'B')
-            return cv::Scalar(255,0,0);
-        else if(c == 'R')
-            return cv::Scalar(0,0,255);
-        else if(c == 'W')
-            return cv::Scalar(255,255,255);
-        else if(c == 'Y')
-            return cv::Scalar(0,255,255);
-        else if(c == 'O')
-            return cv::Scalar(0,165,255);
-        else
-            return cv::Scalar(0,0,0);
-    }
-    static char detectColor(double r, double g, double b, config& color)
-    {
-        char c = ' ';
-        double mais_proximo = 1000;
-        if(mais_proximo > (std::fabs(r-color.Gr) + std::fabs(g-color.Gg) + std::fabs(b-color.Gb)))
-        {
-            mais_proximo = (std::fabs(r-color.Gr) + std::fabs(g-color.Gg) + std::fabs(b-color.Gb));
-            c = 'G';
-        }
-        if(mais_proximo > (std::fabs(r-color.Rr) + std::fabs(g-color.Rg) + std::fabs(b-color.Rb)))
-        {
-            mais_proximo = (std::fabs(r-color.Rr) + std::fabs(g-color.Rg) + std::fabs(b-color.Rb));
-            c = 'R';
-        }
-        if(mais_proximo > (std::fabs(r-color.Br) + std::fabs(g-color.Bg) + std::fabs(b-color.Bb)))
-        {
-            mais_proximo = (std::fabs(r-color.Br) + std::fabs(g-color.Bg) + std::fabs(b-color.Bb));
-            c = 'B';
-        }
-        if(mais_proximo > (std::fabs(r-color.Or) + std::fabs(g-color.Og) + std::fabs(b-color.Ob)))
-        {
-            mais_proximo = (std::fabs(r-color.Or) + std::fabs(g-color.Og) + std::fabs(b-color.Ob));
-            c = 'O';
-        }
-        if(mais_proximo > (std::fabs(r-color.Yr) + std::fabs(g-color.Yg) + std::fabs(b-color.Yb)))
-        {
-            mais_proximo = (std::fabs(r-color.Yr) + std::fabs(g-color.Yg) + std::fabs(b-color.Yb));
-            c = 'Y';
-        }
-        if(mais_proximo > (std::fabs(r-color.Wr) + std::fabs(g-color.Wg) + std::fabs(b-color.Wb)))
-        {
-            mais_proximo = (std::fabs(r-color.Wr) + std::fabs(g-color.Wg) + std::fabs(b-color.Wb));
-            c = 'W';
-        }
-        return c;
-    }
+    return squares;
 }
-using namespace cv;
-using namespace std;
+static cv::Scalar colorForLine(char c)
+{
+    if(c == 'G')
+        return cv::Scalar(0,255,0);
+    else if(c == 'B')
+        return cv::Scalar(255,0,0);
+    else if(c == 'R')
+        return cv::Scalar(0,0,255);
+    else if(c == 'W')
+        return cv::Scalar(255,255,255);
+    else if(c == 'Y')
+        return cv::Scalar(0,255,255);
+    else if(c == 'O')
+        return cv::Scalar(0,165,255);
+    else
+        return cv::Scalar(0,0,0);
+}
+static char detectColor(double r, double g, double b, config& color)
+{
+    char c = ' ';
+    double mais_proximo = 1000;
+    if(mais_proximo > (std::fabs(r-color.Gr) + std::fabs(g-color.Gg) + std::fabs(b-color.Gb)))
+    {
+        mais_proximo = (std::fabs(r-color.Gr) + std::fabs(g-color.Gg) + std::fabs(b-color.Gb));
+        c = 'G';
+    }
+    if(mais_proximo > (std::fabs(r-color.Rr) + std::fabs(g-color.Rg) + std::fabs(b-color.Rb)))
+    {
+        mais_proximo = (std::fabs(r-color.Rr) + std::fabs(g-color.Rg) + std::fabs(b-color.Rb));
+        c = 'R';
+    }
+    if(mais_proximo > (std::fabs(r-color.Br) + std::fabs(g-color.Bg) + std::fabs(b-color.Bb)))
+    {
+        mais_proximo = (std::fabs(r-color.Br) + std::fabs(g-color.Bg) + std::fabs(b-color.Bb));
+        c = 'B';
+    }
+    if(mais_proximo > (std::fabs(r-color.Or) + std::fabs(g-color.Og) + std::fabs(b-color.Ob)))
+    {
+        mais_proximo = (std::fabs(r-color.Or) + std::fabs(g-color.Og) + std::fabs(b-color.Ob));
+        c = 'O';
+    }
+    if(mais_proximo > (std::fabs(r-color.Yr) + std::fabs(g-color.Yg) + std::fabs(b-color.Yb)))
+    {
+        mais_proximo = (std::fabs(r-color.Yr) + std::fabs(g-color.Yg) + std::fabs(b-color.Yb));
+        c = 'Y';
+    }
+    if(mais_proximo > (std::fabs(r-color.Wr) + std::fabs(g-color.Wg) + std::fabs(b-color.Wb)))
+    {
+        mais_proximo = (std::fabs(r-color.Wr) + std::fabs(g-color.Wg) + std::fabs(b-color.Wb));
+        c = 'W';
+    }
+    return c;
+}
+
 int thresh = 50, N = 10;
 const char* wndname = "After Detection";
 static double angle( Point pt1, Point pt2, Point pt0 )
@@ -195,7 +194,7 @@ static void findSquares( const Mat& image, vector<vector<Point> >& squares )
 static std::vector<double> drawSquares( Mat& image, vector<vector<Point> >& squares, vector<std::string>& colors, config& conf)
 {
     std::vector<double> result(squares.size()*3);
-    squares = teste::ordenaCubinho(squares);
+    squares = ordenaCubinho(squares);
     int npoints = 4;
     Point pts[1][4];
     Scalar average;
@@ -227,9 +226,9 @@ static std::vector<double> drawSquares( Mat& image, vector<vector<Point> >& squa
                 result[i*3+1] = g;
                 result[i*3+2] = b;
             }
-            c = teste::detectColor(r,g,b,conf);
-            colors.push_back(teste::Char2StyleSheet(c));
-            polylines(image, &p, &n, 1, true, teste::colorForLine(c), 3, LINE_AA);
+            c = detectColor(r,g,b,conf);
+            colors.push_back(Char2StyleSheet(c));
+            polylines(image, &p, &n, 1, true, colorForLine(c), 3, LINE_AA);
         }
     }
     imshow(wndname, image);
