78ec16e7030dc12fae6cc295b4cbba2b3ebd8303
Craviee <craviee@pm.me>
2020-02-15 18:38:42 +0100

    Updating square class

---
+++ cube/square.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <QPushButton>
+#include "color.h"
+
+class Square
+{
+    public:
+        Square(){}
+        Square(QPushButton* squarePiece) : squarePiece{squarePiece}{}
+        Color getColor();
+        void setColor(Color color);
+    private:
+        QPushButton* squarePiece;
+
+};
