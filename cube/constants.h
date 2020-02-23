0440e1c707038d80a3304bc3d653ba22aec5786e
Craviee <craviee@pm.me>
2020-02-23 16:43:28 +0100

    Creating constants file

---
+++ cube/constants.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define CONFIG_FILE "ColorValues.config"
+
+#define CALIBRATION_HUE_VALUES_QUANTITY 6*3
+
+#define SQUARES_PER_FACE 9
+
+// WEBCAM_ID is 0 if you have just one webcam, if it has more than one (i.e notebook) you should change this
+#define WEBCAM_ID 0
+
+#define CALIBRATION_PICTURES_PER_FACE 100
+
+#define RANDOM_ROTATIONS 30
