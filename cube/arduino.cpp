095fb5192a34131cda2c75871b28cff9833127de
Craviee <craviee@pm.me>
2020-02-25 12:40:12 +0100

    Implementing delayByCommand function

--- cube/arduino.cpp
+++ cube/arduino.cpp
@@ -35,19 +35,29 @@ bool Arduino::isAvailable()
 
 void Arduino::delayByCommand(Command command)
 {
-    
+    switch (command)
+    {
+        // Start with UP, so no delay
+        case Command::MOVE_UP: Utils::delay(0); break;
+        
+        case Command::MOVE_FRONT: Utils::delay(5000); break;
+        case Command::MOVE_DOWN: Utils::delay(5000); break;
+        case Command::MOVE_BACK: Utils::delay(5000); break;
+        case Command::MOVE_RIGHT: Utils::delay(9000); break;
+        case Command::MOVE_LEFT: Utils::delay(7000); break;
+    }
 }
 
 std::string Arduino::command2str(Command command)
 {
     switch (command)
     {
-        case Command::MOVE_UP: return "01";
-        case Command::MOVE_FRONT: return "02";
-        case Command::MOVE_DOWN: return "03";
-        case Command::MOVE_BACK: return "04";
-        case Command::MOVE_RIGHT: return "05";
-        case Command::MOVE_LEFT: return "06";
+        case Command::MOVE_UP: return "00";
+        case Command::MOVE_FRONT: return "01";
+        case Command::MOVE_DOWN: return "02";
+        case Command::MOVE_BACK: return "03";
+        case Command::MOVE_RIGHT: return "04";
+        case Command::MOVE_LEFT: return "05";
         default: return "ERROR";
     }
 }
