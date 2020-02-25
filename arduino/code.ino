0d421535c65f270bc011f4e7eac949d96cf5f081
Craviee <craviee@pm.me>
2020-02-25 12:51:18 +0100

    Translating code to english

--- arduino/code.ino
+++ arduino/code.ino
@@ -1,321 +1,313 @@
 #include <Servo.h>
 
-Servo garra;
+Servo claw;
 Servo base;
-int base_p;
 
-#define vir_esq 58
-#define vir 60
-#define rot 31
-#define del 2000
-#define ang 76
+#define turn_left_degrees 58
+#define turn_right_degrees 60
+#define rotation_degrees 31
+#define delay_time 2000
+#define turn_cube 76
 
 void setup()
 {
-  // Define Garra no pino 6
-  garra.attach(6);
-  // Define Base no pino 11
+  // Define claw on pin 6
+  claw.attach(6);
+  // Define Base on pin 11
   base.attach(11);
-  // Define clock do Arduino
+  // Define Arduino's clock
   Serial.begin(9600);
-  // Inicializa Garra e Base
-  inicializa();
+  // Init claw and base
+  init();
 }
 
 void loop()
 {
   if (Serial.available())
   {
-    // Recebe comando a ser executado
+    // Receive Command
     char char_comando = Serial.read();
     int comando = Serial.parseInt();
     Serial.println(comando);
     switch(comando)
     {
-      case 1: // vai do up pro front // 5s
-        vira(vir);
+      case 1: // from up to front // 5s
+        turn(turn_right_degrees);
         break;
-      case 2: // vai do front pro down // 5s
-        vira(vir);
+      case 2: // from front to down // 5s
+        turn(turn_right_degrees);
         break;
-      case 3: // vai do down pro back // 5s
-        vira(vir);
+      case 3: // from down to back // 5s
+        turn(turn_right_degrees);
         break;
-      case 4: // vai do back pro right // 9s
-        vira(vir);
-        vira(vir);
-        vira_base_esquerda();
-        vira(vir_esq);
-        vira_base_centro();
+      case 4: // from back to right // 9s
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
+        turn_base_left();
+        turn(turn_left_degrees);
+        turn_base_right();
         break;
-      case 5: // vai do right pro left // 7s
-        vira_base_esquerda();
-        vira(vir_esq);
-        vira(vir_esq);
-        vira_base_centro();
+      case 5: // from right to left // 7s
+        turn_base_left();
+        turn(turn_left_degrees);
+        turn(turn_left_degrees);
+        turn_base_right();
         break;
-      case 6: // retorna do left pro up // 12s
-        vira_base_esquerda();
-        vira(vir_esq);
-        vira_base_centro();
-        vira(vir);
-        vira(vir);
-        vira(vir);
+      case 6: // retorna do left to up // 12s
+        turn_base_left();
+        turn(turn_left_degrees);
+        turn_base_right();
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
         break;
       case 7: //U // 35s
-        vira(vir);
-        vira(vir);
-        rotaciona(rot);
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
+        rotate_clockwise(rotation_degrees);
         
-        vira(vir);
-        vira(vir);
-        vira(vir);
-        vira_base_esquerda();
-        vira(vir_esq);
-        vira_base_centro();
-        vira(vir);
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
+        turn_base_left();
+        turn(turn_left_degrees);
+        turn_base_right();
+        turn(turn_right_degrees);
         
-        vira(vir);
-        vira(vir);
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
         break;
       case 8: //U' // 20s
-        vira(vir);
-        vira(vir);
-        rotacionaInv(rot);
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
+        rotate_anticlockwise(rotation_degrees);
         
-        vira(vir);
-        vira_base_esquerda();
-        vira(vir_esq);
-        vira_base_centro();
+        turn(turn_right_degrees);
+        turn_base_left();
+        turn(turn_left_degrees);
+        turn_base_right();
         
-        vira(vir);
+        turn(turn_right_degrees);
         break;
       case 9: //F // 35s
-        vira(vir);
-        vira(vir);
-        vira(vir);
-        rotaciona(rot);
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
+        rotate_clockwise(rotation_degrees);
         
-        vira(vir);
-        vira(vir);
-        vira(vir);
-        vira_base_esquerda();
-        vira(vir_esq);
-        vira_base_centro();
-        vira(vir);
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
+        turn_base_left();
+        turn(turn_left_degrees);
+        turn_base_right();
+        turn(turn_right_degrees);
         
-        vira(vir);
+        turn(turn_right_degrees);
         break;
       case 10: //F' // 20s
-        vira(vir);
-        vira(vir);
-        vira(vir);
-        rotacionaInv(rot);
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
+        rotate_anticlockwise(rotation_degrees);
 
-        vira(vir);
-        vira_base_esquerda();
-        vira(vir_esq);
-        vira_base_centro();
+        turn(turn_right_degrees);
+        turn_base_left();
+        turn(turn_left_degrees);
+        turn_base_right();
 
         break;
       case 11: //D // 31s
-        rotaciona(rot);
+        rotate_clockwise(rotation_degrees);
 
-        vira(vir);
-        vira_base_esquerda();
-        vira(vir_esq);
-        vira(vir_esq);
-        vira(vir_esq);
-        vira_base_centro();
-        vira(vir);
-        vira(vir);
-        vira(vir);
+        turn(turn_right_degrees);
+        turn_base_left();
+        turn(turn_left_degrees);
+        turn(turn_left_degrees);
+        turn(turn_left_degrees);
+        turn_base_right();
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
         
         break;
       case 12: //D' // 20s
-        rotacionaInv(rot);
-        vira(vir);
-        vira_base_esquerda();
-        vira(vir_esq);
-        vira_base_centro();
-        vira(vir);
-        vira(vir);
-        vira(vir);
+        rotate_anticlockwise(rotation_degrees);
+        turn(turn_right_degrees);
+        turn_base_left();
+        turn(turn_left_degrees);
+        turn_base_right();
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
         break;
       case 13: //B // 27s
-        vira(vir);
-        rotaciona(rot);
+        turn(turn_right_degrees);
+        rotate_clockwise(rotation_degrees);
 
-        vira(vir);
-        vira(vir);
-        vira(vir);
-        vira_base_esquerda();
-        vira(vir_esq);
-        vira_base_centro();
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
+        turn_base_left();
+        turn(turn_left_degrees);
+        turn_base_right();
         break;
       case 14: //B' // 19s
-        vira(vir);
-        rotacionaInv(rot);
+        turn(turn_right_degrees);
+        rotate_anticlockwise(rotation_degrees);
 
-        vira(vir);
-        vira_base_esquerda();
-        vira(vir_esq);
-        vira_base_centro();
+        turn(turn_right_degrees);
+        turn_base_left();
+        turn(turn_left_degrees);
+        turn_base_right();
         
-        vira(vir);
-        vira(vir);
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
         break;
       case 15: //R // 39s
-        vira_base_esquerda();
-        vira(vir_esq);
-        vira(vir_esq);
-        vira(vir_esq);
-        vira_base_centro();
-        rotaciona(rot);
+        turn_base_left();
+        turn(turn_left_degrees);
+        turn(turn_left_degrees);
+        turn(turn_left_degrees);
+        turn_base_right();
+        rotate_clockwise(rotation_degrees);
         
-        vira(vir);
-        vira(vir);
-        vira(vir);
-        vira_base_esquerda();
-        vira(vir_esq);
-        vira_base_centro();
-        vira(vir);
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
+        turn_base_left();
+        turn(turn_left_degrees);
+        turn_base_right();
+        turn(turn_right_degrees);
         
-        vira_base_esquerda();
-        vira(vir_esq);
-        vira_base_centro();
+        turn_base_left();
+        turn(turn_left_degrees);
+        turn_base_right();
         break;
       case 16: //R' // 29s
-        vira_base_esquerda();
-        vira(vir_esq);
-        vira(vir_esq);
-        vira(vir_esq);
-        vira_base_centro();
-        rotacionaInv(rot);
+        turn_base_left();
+        turn(turn_left_degrees);
+        turn(turn_left_degrees);
+        turn(turn_left_degrees);
+        turn_base_right();
+        rotate_anticlockwise(rotation_degrees);
 
-        vira(vir);
-        vira(vir);
-        vira_base_esquerda();
-        vira(vir_esq);
-        vira(vir_esq);
-        vira(vir_esq);
-        vira_base_centro();
-        vira(vir);
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
+        turn_base_left();
+        turn(turn_left_degrees);
+        turn(turn_left_degrees);
+        turn(turn_left_degrees);
+        turn_base_right();
+        turn(turn_right_degrees);
         break;
       case 17: //L // 39s
-        vira_base_esquerda();
-        vira(vir_esq);
-        vira_base_centro();
-        rotaciona(rot);
+        turn_base_left();
+        turn(turn_left_degrees);
+        turn_base_right();
+        rotate_clockwise(rotation_degrees);
 
-        vira(vir);
-        vira(vir);
-        vira(vir);
-        vira_base_esquerda();
-        vira(vir_esq);
-        vira_base_centro();
-        vira(vir);
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
+        turn(turn_right_degrees);
+        turn_base_left();
+        turn(turn_left_degrees);
+        turn_base_right();
+        turn(turn_right_degrees);
         
-        vira_base_esquerda();
-        vira(vir_esq);
-        vira(vir_esq);
-        vira(vir_esq);
-        vira_base_centro();
+        turn_base_left();
+        turn(turn_left_degrees);
+        turn(turn_left_degrees);
+        turn(turn_left_degrees);
+        turn_base_right();
         break;
       case 18: //L'// 21s
-        vira_base_esquerda();
-        vira(vir_esq);
-        vira_base_centro();
-        rotacionaInv(rot);
+        turn_base_left();
+        turn(turn_left_degrees);
+        turn_base_right();
+        rotate_anticlockwise(rotation_degrees);
         
-        vira_base_esquerda();
-        vira(vir_esq);
-        vira(vir_esq);
-        vira(vir_esq);
-        vira_base_centro();
-        vira(vir);
+        turn_base_left();
+        turn(turn_left_degrees);
+        turn(turn_left_degrees);
+        turn(turn_left_degrees);
+        turn_base_right();
+        turn(turn_right_degrees);
         break;
     }
   }
 }
 
-void vira_base_esquerda()
+void turn_base_left()
 {
   base.write(5);
-  delay(del); 
+  delay(delay_time); 
 }
 
-void vira_base_centro()
+void turn_base_right()
 {
   base.write(90);
-  delay(del);
+  delay(delay_time);
 }
 
 
-void inicializa()
+void init()
 {
-  garra.write(0);
+  claw.write(0);
   base.write(90);
-  base_p = 90;
-  delay(del);
+  delay(delay_time);
 }
 
-void vira(int n)
+void turn(int n)
 {
-  garra.write(n);
-  delay(del);
-  garra.write(0);
-  delay(del);
+  claw.write(n);
+  delay(delay_time);
+  claw.write(0);
+  delay(delay_time);
 }
 
-void rotacionaInv(int n)
+void rotate_anticlockwise(int n)
 {
   base.write(180);
-  delay(del);
-  garra.write(n);
-  delay(del);
-  base.write(ang);
-  base_p = 0;
-  delay(del);
-  garra.write(0);
-  delay(del);
+  delay(delay_time);
+  claw.write(n);
+  delay(delay_time);
+  base.write(turn_cube);
+  delay(delay_time);
+  claw.write(0);
+  delay(delay_time);
   base.write(90);
-  base_p = 90;
-  delay(del);
+  delay(delay_time);
 }
 
-void rotaciona(int n)
+void rotate_clockwise(int n)
 {
   base.write(180);
-  delay(del);
-  garra.write(n);
-  delay(del);
-  base.write(ang);
-  base_p = 0;
-  delay(del);
-  garra.write(0);
-  delay(del);
+  delay(delay_time);
+  claw.write(n);
+  delay(delay_time);
+  base.write(turn_cube);
+  delay(delay_time);
+  claw.write(0);
+  delay(delay_time);
   base.write(180);
-  delay(del);
-  garra.write(n);
-  delay(del);
-  base.write(ang);
-  base_p = 0;
-  delay(del);
-  garra.write(0);
-  delay(del);
+  delay(delay_time);
+  claw.write(n);
+  delay(delay_time);
+  base.write(turn_cube);
+  delay(delay_time);
+  claw.write(0);
+  delay(delay_time);
   base.write(180);
-  delay(del);
-  garra.write(n);
-  delay(del);
-  base.write(ang);
-  base_p = 0;
-  delay(del);
-  garra.write(0);
-  delay(del);
+  delay(delay_time);
+  claw.write(n);
+  delay(delay_time);
+  base.write(turn_cube);
+  delay(delay_time);
+  claw.write(0);
+  delay(delay_time);
   base.write(90);
-  base_p = 90;
-  delay(del);
+  delay(delay_time);
 }
 
 
